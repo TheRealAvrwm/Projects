e010c5de45273b5df64e9daeebd506c7d3563f8f
TheRealAvrwm <161508444+TheRealAvrwm@users.noreply.github.com>
2025-04-01 00:16:10 -0700

    Update tester.lua

--- tester.lua
+++ tester.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/TheRealAvrwm/Zephyr-v2/refs/heads/main/Zephyr%20v2%20%5BPaid%20executors%5D'),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/TheRealAvrwm/mvsd/refs/heads/main/Zephyr%20V2'),true))()
